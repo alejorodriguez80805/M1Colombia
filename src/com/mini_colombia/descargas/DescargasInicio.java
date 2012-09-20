78ca2e91f8d0bf9118d3e5a84a433b0673110f75
alejo80805 <alejo80805@hotmail.com>
2012-09-20 10:10:10 -0500

    PRoblema MediaPlayer resuelto (Los dispositivos que tengan un api menor al 4.0 no pueden acceder a esa parte de la aplicacion) y imagenes inicio descargas ajustadas

--- src/com/mini_colombia/descargas/DescargasInicio.java
+++ src/com/mini_colombia/descargas/DescargasInicio.java
@@ -8,17 +8,25 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.mini_colombia.R;
+import com.mini_colombia.servicios.Resize;
 
 public class DescargasInicio  extends ActivityGroup
 {
+	private static final int VERSION_ICE_CREAM_SANDWICH=14;
+	
 	public static DescargasInicio grupoDescargas;
 	
 	public ArrayList<View> historialViews;
@@ -35,6 +43,23 @@ public class DescargasInicio  extends ActivityGroup
 		Typeface tipoMini = Typeface.createFromAsset(getAssets(), "fonts/mibd.ttf");
 		TextView titulo = (TextView)findViewById(R.id.tituloDescargasInicio);
 		titulo.setTypeface(tipoMini);
+		
+		Resources res = getResources();
+		ImageButton botonWallpapers = (ImageButton) findViewById(R.id.descargasImagenWallpapers);
+		float anchoImagen = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 296, res.getDisplayMetrics());
+		Bitmap wallpapersPreliminar = BitmapFactory.decodeResource(res, R.drawable.thumbnail_wallpaper);
+		Bitmap wallpapersFinal = Resize.resizeBitmap(wallpapersPreliminar, (int)Math.round(anchoImagen*0.45),(int)anchoImagen);
+		botonWallpapers.setBackgroundDrawable(null);
+		botonWallpapers.setImageBitmap(wallpapersFinal);
+		
+		ImageButton botonRingtones = (ImageButton) findViewById(R.id.descargasImagenRingtones);
+		Bitmap ringtonesPreliminar = BitmapFactory.decodeResource(res, R.drawable.thumbnail_ringtones);
+		Bitmap ringtonesFinal = Resize.resizeBitmap(ringtonesPreliminar, (int)Math.round(anchoImagen*0.45), (int)anchoImagen);
+		botonRingtones.setBackgroundDrawable(null);
+		botonRingtones.setImageBitmap(ringtonesFinal);
+		
+		
+		
 	}
 	
 	
@@ -72,10 +97,32 @@ public class DescargasInicio  extends ActivityGroup
 	{
 		if(hayConexionInternet())
 		{
-			Intent i = new Intent(DescargasInicio.this, DescargasRingtones.class);
-			i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-			View v1 = getLocalActivityManager().startActivity("", i).getDecorView();
-			reemplazarView(v1);
+			if(Integer.valueOf(android.os.Build.VERSION.SDK_INT)>=VERSION_ICE_CREAM_SANDWICH)
+			{
+				Intent i = new Intent(DescargasInicio.this, DescargasRingtones.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				View v1 = getLocalActivityManager().startActivity("", i).getDecorView();
+				reemplazarView(v1);
+			}
+			else
+			{
+				AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+				alertBuilder.setMessage("Necesitas una version mas avanzada de Android para acceder a esta seccion.");
+				alertBuilder.setCancelable(false);
+				alertBuilder.setNeutralButton("Aceptar", new DialogInterface.OnClickListener() 
+				{
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) 
+					{
+						onCreate(null);
+					}
+				});
+				AlertDialog alerta = alertBuilder.create();
+				alerta.show();
+			}
+			
+
 		}
 		else
 		{
